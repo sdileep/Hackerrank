daef72158786c138f1574fcffbfa6bf01dd349a3
Sunil Gopinath <sxg282@gmail.com>
2018-01-29 12:39:34 +1100

    correcte formatting

--- implementation/apple_orange.go
+++ implementation/apple_orange.go
@@ -1,41 +1,38 @@
 package main
+
 import "fmt"
 
 func main() {
-    //Enter your code here. Read input from STDIN. Print output to STDOUT
-    var s, t, a, b, m, n, mm, nn int
-    fmt.Scan( & s)
-    fmt.Scan( & t)
-    fmt.Scan( & a)
-    fmt.Scan( & b)
-    fmt.Scan( & m)
-    fmt.Scan( & n)
+	//Enter your code here. Read input from STDIN. Print output to STDOUT
+	var s, t, a, b, m, n, mm, nn int
+	fmt.Scan(&s)
+	fmt.Scan(&t)
+	fmt.Scan(&a)
+	fmt.Scan(&b)
+	fmt.Scan(&m)
+	fmt.Scan(&n)
 
-    var apples[] int
-    var oranges[] int
-    for i: = 0;
-    i < m;
-    i++{
-        fmt.Scan( & mm)
-        apples = append(apples, mm)
-    }
+	var apples []int
+	var oranges []int
+	for i := 0; i < m; i++ {
+		fmt.Scan(&mm)
+		apples = append(apples, mm)
+	}
 
-    for i: = 0;
-    i < n;
-    i++{
-        fmt.Scan( & nn)
-        oranges = append(oranges, nn)
-    }
+	for i := 0; i < n; i++ {
+		fmt.Scan(&nn)
+		oranges = append(oranges, nn)
+	}
 
-    fmt.Printf("%d\n%d\n", countFruit(apples, a, s, t), countFruit(oranges, b, s, t))
+	fmt.Printf("%d\n%d\n", countFruit(apples, a, s, t), countFruit(oranges, b, s, t))
 }
 
-func countFruit(arr[] int, a, s, t int) int {
-    var c int
-    for _, f: = range arr {
-        if a + f >= s && a + f <= t {
-            c++
-        }
-    }
-    return c
+func countFruit(arr []int, a, s, t int) int {
+	var c int
+	for _, f := range arr {
+		if a+f >= s && a+f <= t {
+			c++
+		}
+	}
+	return c
 }
