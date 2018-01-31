e504981ae8a471f0dd24be9b1d537c31b2cba2bd
Sunil Gopinath <sxg282@gmail.com>
2018-02-01 01:13:37 +1100

    Update bon_apetit.go

--- implementation/bon_apetit.go
+++ implementation/bon_apetit.go
@@ -2,28 +2,21 @@ package main
 import "fmt"
 
 func main() {
- //Enter your code here. Read input from STDIN. Print output to STDOUT
-    var n, k, c, b int
-    fmt.Scan(&n)
-    fmt.Scan(&k)
-    var arr []int
-    
-    for i := 0; i < n; i++ {
-        fmt.Scan(&c)
-        arr = append(arr, c)
-    }
-    fmt.Scan(&b)
-    
-    count := 0
-    for i := 0; i < n; i++ {
-        if i != k {
-            count+=arr[i]
-        }
-    }
-    charge := count / 2
-    if charge == b {
-        fmt.Println("Bon Appetit")
-    } else {
-        fmt.Println(b - charge)
-    }
+ fmt.Scan(&b)
+	fs := fairSplit(arr, k)
+	if b != fs {
+		fmt.Println(b - fs)
+	} else {
+		fmt.Println("Bon Appetit")
+	}
+}
+
+func fairSplit(arr []int, k int) int {
+	var t int
+	for i, c := range arr {
+		if i != k {
+			t += c
+		}
+	}
+	return t / 2
 }
